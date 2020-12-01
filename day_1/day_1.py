4730967612d70790a339d773d8ce445f66e1cc6f
Mary Knize <maryknize@gmail.com>
2020-12-01 14:06:17 -0500

    Day 1 refactor

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -2,6 +2,9 @@ input = open("input.txt", 'r')
 lines = input.readlines()
 
 # Part 1
+# Loops through the provided numbers
+# and loops through all numbers before
+# until finding the numbers that equal 2020.
 for (i, line) in enumerate(lines):
     for j in range(0, i):
         a = int(line)
@@ -13,6 +16,10 @@ for (i, line) in enumerate(lines):
             break
 
 # Part 2
+# The same as part 1, but with an additional loop
+# that loops through the numbers again.
+# Unless all the numbers are at the end of the list,
+# this should be fast-ish as it breaks when 2020 is found.
 for (i, line) in enumerate(lines):
     for j in range(0, i):
         for k in range(0, j):
--- day_1/day_1_refactor.py
+++ day_1/day_1_refactor.py
@@ -1,17 +1,23 @@
 from itertools import combinations
+from functools import reduce
+
 input = open("input.txt", 'r')
 lines = input.readlines()
 
 
 # This makes part 1 faster by removing the second loop.
+# It doesn't even loop through fully once.
+# It checks to see if the current number x has been added to dictionary d
+# as the difference between n and a previous number.
+# If not, it adds n - x: x to d.
 def find_two_entries(n):
     d = {}
     for line in lines:
         x = int(line)
-        d[n - x] = x
         if x in d:
             print(x, d[x])
             return x * d[x]
+        d[n - x] = x
 
 
 print(find_two_entries(2020))
@@ -19,6 +25,7 @@ print(find_two_entries(2020))
 
 # Creates every 3-number combination with itertools.combinations,
 # then finds the combo that adds up to n.
+# https://www.geeksforgeeks.org/python-find-all-triplets-in-a-list-with-given-sum/
 def find_three_entries(n):
     nums = list(map(lambda x: int(x), lines))
     combos = list(combinations(nums, 3))
@@ -29,3 +36,17 @@ def find_three_entries(n):
 
 
 print(find_three_entries(2020))
+
+
+# Multi-use function that can be used to find any number of combinations
+# that add up to n. Returns the product of the combo.
+def find_entries(n, num_entries):
+    combos = list(combinations([int(x) for x in lines], num_entries))
+    for c in combos:
+        if sum(c) == n:
+            print(c)
+            return reduce(lambda a, b: a * b, c)
+
+
+print(find_entries(2020, 2))
+print(find_entries(2020, 3))
