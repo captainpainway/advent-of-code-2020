c3ecfd6d006a743a8f620e9a2b1e7d6c4f670dc4
Mary Knize <maryknize@gmail.com>
2020-12-06 12:36:07 -0500

    Fixed some breakage in Day 6

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,20 +1,28 @@
 # https://adventofcode.com/2020/day/6
 
-puzzle_input = open("input.txt", "r")
-txt = puzzle_input.read().replace("\n\n", "---").replace("\n", '').strip()
-lines = txt.split("---")
-
-
-def individual_yes_answers(data):
+# Put each line into a set, get the length of the set,
+# and sum the counts in the list.
+def individual_yes_answers():
+    puzzle_input = open("input.txt", "r")
+    txt = puzzle_input.read().replace("\n\n", "---").replace("\n", '').strip()
+    data = txt.split("---")
     dups_removed = [set(line) for line in data]
     counts = [len(arr) for arr in dups_removed]
     return sum(counts)
 
 
-print(individual_yes_answers(lines))
+print(individual_yes_answers())
 
 
-def total_yes_answers(data):
+# Creating a dictionary of letters for each line.
+# If any of the letters in the dictionary are equal to
+# the length of the list created by splitting the line by
+# spaces, it's considered a valid answer.
+# Increment the number of valid answers by 1, and return that value.
+def total_yes_answers():
+    puzzle_input = open("input.txt", "r")
+    txt = puzzle_input.read().replace("\n\n", "---").replace("\n", ' ').strip()
+    data = txt.split("---")
     valid_answers = 0
     for line in data:
         dct = {}
@@ -32,4 +40,4 @@ def total_yes_answers(data):
     return valid_answers
 
 
-print(total_yes_answers(lines))
+print(total_yes_answers())
